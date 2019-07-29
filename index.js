fcfd6f0c5e46299c29d1dbcf53d05a01b6315a91
Phil <philip.schaefer0@gmail.com>
2019-07-29 23:35:36 +0200

    added DB

--- index.js
+++ index.js
@@ -9,8 +9,24 @@ const fs = require('fs');
 const config = require('./config/main.json');
 
 config.env = new Discord.Collection();
+
+// bot and mysql login
+let token;
+let host;
+let user;
+let password;
+let database;
 if (fs.existsSync('./config/test_token.json')) {
-    client.login(token.token);
+  const dev = require('./config/test_token.json');
+  token = dev.token;
+  host = dev.DB_host;
+  user = dev.DB_user;
+  password = dev.DB_passw;
+  database = dev.DB_name;
   } else {
-    client.login(process.env.BOT_TOKEN);
+  token = process.env.BotToken;
+  host = process.env.DB_host;
+  user = process.env.DB_user;
+  password = process.env.DB_passw;
+  database = process.env.DB_name;
   }
